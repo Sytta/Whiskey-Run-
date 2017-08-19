a9fb201f588c847a72ff5d761ce0bf272d7fb5ff
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2017-08-19 14:34:06 -0400

    [Racing UI] Countdown

--- Assets/Scripts/UI/RacingUI.cs
+++ Assets/Scripts/UI/RacingUI.cs
@@ -7,12 +7,30 @@ public class RacingUI : MonoBehaviour
 	[SerializeField] private Text[] playerCurrency;
 	[SerializeField] private Text[] countDown;
 
+	public void InitCountDown(string text)
+	{
+		for (int i = 0; i < countDown.Length; i++)
+		{
+			countDown [i].gameObject.SetActive (true);
+		}
+		SetCountDown (text);
+	}
+
 	public void SetCountDown(string text)
 	{
 		for (int i = 0; i < countDown.Length; i++)
 		{
 			countDown [i].text = text;
 		}
+	}
+
+	public void DisableCountDown()
+	{
+		for (int i = 0; i < countDown.Length; i++)
+		{
+			countDown [i].gameObject.SetActive (false);
+		}
+		SetCountDown ("");
 	}
 }
 
