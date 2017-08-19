acfafae5ad3e67f6a9fff0f5515e26f4437c57ce
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2017-08-19 14:30:44 -0400

    [Racing UI] Fix missing script

---
+++ Assets/Scripts/UI/AbilitySlot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilitySlot : MonoBehaviour
+{
+	[SerializeField] public int slotId;
+	[SerializeField] private Image background;
+	[SerializeField] private Image fill;
+
+	public void SetImage(Sprite image)
+	{
+		background.sprite = image;
+		fill.sprite = image;
+	}
+}
+
---
+++ Assets/Scripts/UI/RacingUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RacingUI : MonoBehaviour
+{
+	[SerializeField] private Text[] playerCurrency;
+	[SerializeField] private Text[] countDown;
+
+	public void SetCountDown(string text)
+	{
+		for (int i = 0; i < countDown.Length; i++)
+		{
+			countDown [i].text = text;
+		}
+	}
+}
+
