a16d2bcf6c11089911f485e997ea22929010dfa4
bolter47 <gendreau.camille101@gmail.com>
2017-08-19 03:20:04 -0400

    Caravan movement script finished

---
+++ Assets/Scripts/CaravanInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravanInput : MonoBehaviour {
+
+	[SerializeField]
+	MotionControl motionControl; // Control over the player's prefab
+
+	string playerInputSuffix;
+
+	/*************************************DELETE THIS AFTER... TESTING ONLY ********************************/
+	private void Start()
+	{
+		// set player one on start until chariot spawn implemented
+		SetPlayer(1);
+	}
+
+	public void SetPlayer(int playerNumber)
+	{
+		if (playerNumber < 1)
+		{
+			playerNumber = 1;
+		}
+		playerInputSuffix = "_P" + playerNumber;
+	}
+
+	private void Update()
+	{
+		// take input movement
+		motionControl.AccelerateToward(new Vector3(Input.GetAxis("Horizontal" + playerInputSuffix), 0, Input.GetAxis("Vertical" + playerInputSuffix)).normalized);
+	}
+}
---
+++ Assets/Scripts/Movement/FloorDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDetector : MonoBehaviour {
+	[SerializeField]
+	MotionControl ReadjustmentController;
+
+	// Use this for initialization
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (collider.CompareTag("ground"))
+		{
+			ReadjustmentController.SetGrounded(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider collider)
+	{
+		if (collider.CompareTag("ground"))
+		{
+			ReadjustmentController.SetGrounded(false);
+		}
+	}
+}
