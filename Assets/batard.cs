38f9483ff769b77cd6fce4a9fc9eaa7b88fa4119
unknown <nadjam@326-H5.nad.qc.ca>
2017-08-19 18:16:00 -0400

    add mesh asset

---
+++ Assets/batard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batard : MonoBehaviour {
+
+	public CaravanInput cart;
+
+	// Use this for initialization
+	void Start () {
+		cart.SetPlayer (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
