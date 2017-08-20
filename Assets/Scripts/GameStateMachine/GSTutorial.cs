94c0c24be453d757151185c5d6830873d8ef5655
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2017-08-20 16:12:07 -0400

    Fix build

--- Assets/Scripts/GameStateMachine/GSTutorial.cs
+++ Assets/Scripts/GameStateMachine/GSTutorial.cs
@@ -9,6 +9,15 @@ public partial class GameStateMachine : MonoBehaviour
 	{
 		public GSTutorial(GameStateMachine owner) : base(owner) { }
 
+		private enum State
+		{
+			CONTROLS,
+			TUTORIAL,
+			END
+		}
+		private State currentState = State.CONTROLS;
+		private bool canControl = true;
+
 		public override void OnEnter()
 		{
             Debug.Log("Going from Tutorial to RaceSetup");
@@ -22,9 +31,41 @@ public partial class GameStateMachine : MonoBehaviour
 			// Hide the tutorial
 		}
 
+		private void RunState()
+		{
+			switch (currentState)
+			{
+			case State.CONTROLS:
+				break;
+			case State.TUTORIAL:
+				break;
+			case State.END:
+				OnClickedPlay ();
+				break;
+			}
+		}
+
+		private void IncrementState()
+		{
+			currentState++;
+			RunState ();
+		}
+
 		public override void OnUpdate(float deltaTime)
 		{
 			// nothing for now.
+			if (Input.GetAxis ("AAbility_P1") > 0.0f || Input.GetAxis ("AAbility_P2") > 0.0f || Input.GetKeyDown (KeyCode.Return))
+			{
+				if (canControl)
+				{
+					canControl = false;
+					IncrementState ();
+				}
+			}
+			else
+			{
+				canControl = true;
+			}
 		}
 
 		public void OnClickedPlay()
