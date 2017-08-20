82e0901a57c696f6a6c69128da068bc70860b272
Yawen <sytta@live.ca>
2017-08-20 13:46:11 -0400

    Disable Ability controller in GSShop

--- Assets/Scripts/GameStateMachine/GSUpgradesMenu.cs
+++ Assets/Scripts/GameStateMachine/GSUpgradesMenu.cs
@@ -21,6 +21,12 @@ public partial class GameStateMachine : MonoBehaviour
             // Show the tutorial
             owner.currentState = this;
 			Shop = Instantiate (GameManager.instance.PrefabManager.Shop);
+
+            // Disable ability controller
+            for (int i = 0; i < GameManager.instance.caravanSpanwer.caravans.Count; i++)
+            {
+                GameManager.instance.caravanSpanwer.caravans[i].GetComponent<AbilityController>().enabled = false;
+            }
         }
 
         public override void OnExit()
@@ -29,6 +35,12 @@ public partial class GameStateMachine : MonoBehaviour
 			Destroy(Shop);
 			readyPlayer1 = false;
 			readyPlayer2 = false;
+
+            // Enable ability controller
+            for (int i = 0; i < GameManager.instance.caravanSpanwer.caravans.Count; i++)
+            {
+                GameManager.instance.caravanSpanwer.caravans[i].GetComponent<AbilityController>().enabled = true;
+            }
         }
 
         public override void OnUpdate(float deltaTime)
