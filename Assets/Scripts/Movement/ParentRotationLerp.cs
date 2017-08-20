04806596ba5c0dfb4db11450f3a6d9f45c6d386d
bolter47 <gendreau.camille101@gmail.com>
2017-08-20 08:39:00 -0400

    Camera slerp for less vomit inducing snaps

---
+++ Assets/Scripts/Movement/ParentRotationLerp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentRotationLerp: MonoBehaviour {
+
+	[SerializeField]
+	float LerpRotationSpeed;
+
+	Quaternion previousRotation, targetRotation;
+
+	// Use this for initialization
+	void Start () {
+		previousRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		previousRotation = transform.rotation = Quaternion.Slerp(previousRotation, transform.parent.localRotation, LerpRotationSpeed * Time.deltaTime);
+	}
+}
