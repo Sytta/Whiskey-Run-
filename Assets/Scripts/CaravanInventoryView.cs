c705b4d2786d67bd80b1bf6ca5e09959ba9a97ca
bolter47 <gendreau.camille101@gmail.com>
2017-08-20 09:16:20 -0400

    Caravan inventory view stup

---
+++ Assets/Scripts/CaravanInventoryView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravanInventoryView : MonoBehaviour {
+	[SerializeField]
+	List<Transform> itemPositions;
+
+	public void AddItem()
+	{
+		// TODO: Get next free item position and spawn item
+	}
+
+	public void RemoveItem()
+	{
+		// TODO: find item position and remove it from caravan
+	}
+
+	public void DropItem()
+	{
+		// TODO: remove the item while spawning a physics version that falls off the caravan.
+	}
+}
