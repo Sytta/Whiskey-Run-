3e6295c7105c07d09245328b6821c067e89c5f07
bolter47 <gendreau.camille101@gmail.com>
2017-08-19 20:41:24 -0400

    Added rotation lock script

---
+++ Assets/Scripts/Movement/RotationLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLock : MonoBehaviour {
+	[SerializeField]
+	bool xLock, yLock, zLock;
+	Vector3 initialRotation;
+	// Use this for initialization
+	void Start () {
+		initialRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		var newRotation = transform.rotation.eulerAngles;
+		float x = (xLock) ? initialRotation.x:newRotation.x;
+		float y = (yLock) ? initialRotation.y:newRotation.y;
+		float z = (zLock) ? initialRotation.z:newRotation.z;
+		transform.rotation = Quaternion.Euler(x, y, z);
+	}
+}
