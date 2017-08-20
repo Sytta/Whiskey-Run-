1952ae928a0e85e63fce2f9c065580b519f4db0c
Nathalie Deviana Bekaert <Nathalie.Bekaert@gmail.com>
2017-08-20 00:08:48 -0400

    [UI] Scoreboard

---
+++ Assets/Scripts/UI/ScoreBoard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+	[SerializeField] Text[] positions;
+	[SerializeField] Text[] positionsBonus;
+	[SerializeField] Text[] nbCrates;
+	[SerializeField] Text[] cratesBonus;
+	[SerializeField] Text[] total;
+	[SerializeField] Button continueButton;
+	[SerializeField] Button quitButton;
+
+	public void SetPosition(int playerIndex, int position)
+	{
+		positions [playerIndex].text = "POSITION : " + position.ToString ();
+	}
+
+	public void SetPositionBonus(int playerIndex, int bonus)
+	{
+		positionsBonus [playerIndex].text = "POSITION BONUS CASH : " + bonus.ToString ();
+	}
+
+	public void SetCrates(int playerIndex, int nCrates)
+	{
+		nbCrates [playerIndex].text = "NUMBER OF CRATES : " + nCrates.ToString ();
+	}
+
+	public void SetCratesBonus(int playerIndex, int bonus)
+	{
+		cratesBonus [playerIndex].text = "CRATES INCOME : " + bonus.ToString ();
+	}
+
+	public void SetTotalIncome(int playerIndex, int income)
+	{
+		total [playerIndex].text = "TOTAL INCOME - " + income.ToString ();
+	}
+}
+
