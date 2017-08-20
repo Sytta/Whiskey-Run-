27c7ae9ed5b3c8a9427a8967376b8118e7d3a6b8
bolter47 <gendreau.camille101@gmail.com>
2017-08-20 12:38:09 -0400

    bstacle prefab and Caravan prefab null ref fix

--- Assets/Scripts/RoadObstacle.cs
+++ Assets/Scripts/RoadObstacle.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadObstacle : MonoBehaviour {
-
-	bool armed = false;
-
-
-	private void Start()
-	{
-
-	}
+	public bool armed = false;
 
 	private void OnTriggerEnter(Collider other)
 	{
+		if (!armed)
+		{
+			return;
+		}
+
 		var motControl = other.GetComponentInParent<MotionControl>();
-		if (motControl)
+		if (motControl && armed)
 		{
 			motControl.currentSpeed = 0;
 			motControl.AddModifier(-0.25f, -0.25f, 2);
+			Destroy(GetComponent<InventoryWrecker>());
+			armed = false;
 		}
 	}
 }
